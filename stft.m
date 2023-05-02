7e6889e75feaf65b445613ae60333ee599cfb5da
KEGL--TOURNEIX Noemie <Noemie.Kegl--Tourneix@bordeaux-inp.fr>
2023-05-02 16:18:58 +0200

    ajout de la partie graphique des spectrogrammes

--- stft.m
+++ stft.m
@@ -2,18 +2,20 @@ function [X, f, t] = stft(x,w,d,N_fft,Fs)
 
 % Initialisation of parameters 
 N = length(w); % Length of the winded signal
-M = floor(L/d); % Number of frames
-X = zeros(N_fft:M); % Initialisation of the TF
+L = length(x);
+M = floor(L/d) - 1; % Number of frames
+X = zeros(N_fft,M); % Initialisation of the TF
+w = reshape(w, N, 1);
 
 % Compution of TF
 for j = 1:M
-    vect_x = x(j:j+N-1); % Extraction of vectors of x 
+    vect_x = x((j-1)*d + 1:(j-1)*d+N); % Extraction of vectors of x 
     window = w.*vect_x; % vectors winded by window
     X(:,j) = fft(window,N_fft); % Compution of TF
 end
 
 % Expression of f and t
 f = (0:Fs/N_fft:Fs);
-t = (0:d:M);
+t = (0:d/Fs:L/Fs);
 
 end
