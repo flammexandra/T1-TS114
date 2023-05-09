10f6666218e61d9456393c481a2347d4d8c59080
Alexandra MONY <amony001@enseirb-matmeca.fr>
2023-05-09 19:08:15 +0200

    mfcc

--- part5.m
+++ part5.m
@@ -1,10 +1,22 @@
 close all; % Resets matlab workspace
 close all; % Close every open figure
 clc; % clear the command window
+clear all;
 
-load("data/data_2.mat");
+%% 5 Classification
+
+%% 5.1 Train the classifier
+
+%Question 14
+
+load("data/data_1.mat");
 load("data/train_1.mat");
-[matF] = train_classifier(data_2,train_1,Fs);
+[matF] = train_classifier(data_1,train_1,Fs);
 
 figure;
 plot(matF(:,1));
+
+%Question 15
+load("data/test_1.mat");
+matF_test=train_classifier(data_1,test_1,Fs);
+matF_train=train_classifier(data_1,train_1,Fs);
