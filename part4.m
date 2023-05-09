df3d00c48859a74df218b714bc3a64f15bce77f2
SALLMONE Armela <Armela.Sallmone@bordeaux-inp.fr>
2023-05-09 19:30:36 +0200

    version finale part-4

--- part4.m
+++ part4.m
@@ -2,6 +2,10 @@ close all; % Resets matlab workspace
 close all; % Close every open figure
 clear all;
 
+close all; % Resets matlab workspace
+close all; % Close every open figure
+clear all;
+
 %% Part 4 : Feature extraction
 
 % 4.1 Voiced/Voiceless flag and pitch
@@ -10,90 +14,100 @@ clear all;
 
 % Question 9 : code "isvoiced.m"
 
+
 % Question 10 : 
+% %% Initialisation of some variables
+% 
+% load('data/one1.mat');   
+% load('data/one2.mat');
+% load('data/two1.mat');
+% load('data/two2.mat');
+% 
+% %% Start of your processing
+% 
+% % The pitch features for each frame 
+% one1_compute=compute(one1,Fs);
+% one2_compute=compute(one2,Fs);
+% two1_compute=compute(two1,Fs);
+% two2_compute=compute(two2,Fs);
+% 
+% 
+% %% Display of processing 
+% 
+% 
+% t = tiledlayout(2,2);
+% nexttile
+% plot(one1_compute(1,:),"o","MarkerSize", 8, "LineWidth", 2);
+% hold on;
+% plot(one2_compute(1,:),"x","MarkerSize", 8, "LineWidth", 2);
+% title('Voiced Flag One');
+% legend('One1', 'One2');
+% 
+% nexttile
+% plot(one1_compute(2,:),"o","MarkerSize", 8, "LineWidth", 2);
+% hold on;
+% plot(one2_compute(2,:),"x","MarkerSize", 8, "LineWidth", 2);
+% title('Pitch One');
+% legend('One1', 'One2');
+% 
+% 
+% nexttile
+% plot(two1_compute(1,:),"o","MarkerSize", 8, "LineWidth", 2);
+% hold on;
+% plot(two2_compute(1,:),"x","MarkerSize", 8, "LineWidth", 2);
+% title('Voiced Flag Two');
+% legend('Two1', 'Two2');
+% 
+% nexttile
+% plot(two1_compute(2,:),"o","MarkerSize", 8, "LineWidth", 2);
+% hold on;
+% plot(two2_compute(2,:),"x","MarkerSize", 8, "LineWidth", 2);
+% title('Pitch two');
+% legend('Two1', 'Two2');
 
-    
 
-%one1
+%4.2 MFCC
+
+%Question 11 : code "mfcc.m"
+
+%Question 12: code "mfcc_features.m"
+
+%Question 13 :
+%% Initialisation of some variables
+
 load('data/one1.mat');
-one1_compute=compute(one1,Fs);
-
-
-
-%one2
 load('data/one2.mat');
-one2_compute=compute(one2,Fs);
-
-
-
-t = tiledlayout(2,2);
-nexttile
-plot(one1_compute(1,:),"o","MarkerSize", 8, "LineWidth", 2);
-hold on;
-plot(one2_compute(1,:),"x","MarkerSize", 8, "LineWidth", 2);
-title('Voiced Flag One');
-legend('One1', 'One2');
-
-nexttile
-plot(one1_compute(2,:),"o","MarkerSize", 8, "LineWidth", 2);
-hold on;
-plot(one2_compute(2,:),"x","MarkerSize", 8, "LineWidth", 2);
-title('Pitch One');
-legend('One1', 'One2');
-
-
-%two1
 load('data/two1.mat');
-two1_compute=compute(two1,Fs);
-
-
-%two2
 load('data/two2.mat');
-two2_compute=compute(two2,Fs);
 
-nexttile
-plot(two1_compute(1,:),"o","MarkerSize", 8, "LineWidth", 2);
-hold on;
-plot(two2_compute(1,:),"x","MarkerSize", 8, "LineWidth", 2);
-title('Voiced Flag Two');
-legend('Two1', 'Two2');
-
-nexttile
-plot(two1_compute(2,:),"o","MarkerSize", 8, "LineWidth", 2);
-hold on;
-plot(two2_compute(2,:),"x","MarkerSize", 8, "LineWidth", 2);
-title('Pitch two');
-legend('Two1', 'Two2');
-
-
-%Question 13
-
-% A demander: ne manque t il pas des valeurs... K R et fs?
-load('data/one1.mat');
-load('data/one2.mat');
-N = 0.03*Fs;
+N = 0.03*Fs; %duration of the window 30ms 
+w = hamming(N);
 d = N/2;
 P = 20;
 N_fft = 1024;
 N_mfcc = 20;
-w = hamming(N);
 
-[mfcc1] = mfcc_arm(one1,w,d,N_fft,N_mfcc, Fs, P);
+%% Start of your processing
 
-[mfcc2] = mfcc_arm(one2,w,d,N_fft,N_mfcc, Fs, P);
+% mfcc coefficients foe each signal 
+[mfcc_one1] = mfcc(one1,w,d,N_fft,N_mfcc, Fs, P);
+[mfcc_one2] = mfcc(one2,w,d,N_fft,N_mfcc, Fs, P);
+[mfcc_two1] = mfcc(one2,w,d,N_fft,N_mfcc, Fs, P);
+[mfcc_one2] = mfcc(one2,w,d,N_fft,N_mfcc, Fs, P);
 
-[mfcc_feat1] = mfcc_features(one1,w,d,N_fft, N_mfcc, Fs, P);
-[mfcc_feat2] = mfcc_features(one2,w,d,N_fft, N_mfcc, Fs, P);
+% mfcc
+[mfcc_feat_one1] = mfcc_features(one1,w,d,N_fft, N_mfcc, Fs, P);
+[mfcc_feat_one2] = mfcc_features(one2,w,d,N_fft, N_mfcc, Fs, P);
+[mfcc_feat_two1] = mfcc_features(two1,w,d,N_fft, N_mfcc, Fs, P);
+[mfcc_feat_two2] = mfcc_features(two2,w,d,N_fft, N_mfcc, Fs, P);
 
+%% Display of processing 
 
 figure;
-plot(mfcc1);
+plot(mfcc_one1, mfcc_one2);
+hold on;
+plot(mfcc_feat_one1, mfcc_feat_one2);
 
 figure;
-plot(mfcc2);
+plot(mfcc_feat_one1(2:end));
 
-figure;
-
-plot(mfcc_feat1(2:end));
-figure;
-plot(mfcc_feat2(2:end));
